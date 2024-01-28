af550799eda8c6b96ded3563db740ff2b9728190
Benjamin Ting <chbting@gmail.com>
2024-01-29 02:31:43 +0800

    Updated project name

--- src/Paths.kt
+++ src/Paths.kt
@@ -3,7 +3,7 @@ class Paths {
         const val MAPPED_ROUTES_SOURCE_PATH = "resources/BusRoute_GEOJSON.zip"
         const val BUS_STOPS_SOURCE_PATH = "resources/CoordinateofBusStopLocation_GEOJSON.zip"
 
-        const val DB_EXPORT_PATH = "resources/HKBuserDB.json.gz"
+        const val DB_EXPORT_PATH = "resources/UpBusHK-DB.json.gz"
         const val ROUTE_INFO_EXPORT_PATH = "resources/route_info.json.gz"
     }
 }

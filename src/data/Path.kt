472bedc78e7768236e6262d66e76855971cd3e58
Benjamin Ting <chbting@gmail.com>
2024-01-31 13:52:29 +0800

    Change path id/coords sequence

--- src/data/Path.kt
+++ src/data/Path.kt
@@ -1,7 +1,8 @@
 package data
 
+import com.beust.klaxon.Json
 import com.beust.klaxon.Klaxon
 
-data class Path(val id: Int, val coords: List<List<Double>>) {
+data class Path(@Json(index = 1) val id: Int, @Json(index = 2) val coords: List<List<Double>>) {
     fun toJson() = Klaxon().toJsonString(this)
 }

acb4815653787499f574ed73d482d22629bc8515
Benjamin Ting <chbting@gmail.com>
2024-02-01 12:47:49 +0800

    renamed Path's id to pathId

--- src/data/Path.kt
+++ src/data/Path.kt
@@ -3,6 +3,6 @@ package data
 import com.beust.klaxon.Json
 import com.beust.klaxon.Klaxon
 
-data class Path(@Json(index = 1) val id: Int, @Json(index = 2) val coords: List<List<Double>>) {
+data class Path(@Json(index = 1) val pathId: Int, @Json(index = 2) val coords: List<List<Double>>) {
     fun toJson() = Klaxon().toJsonString(this)
 }
